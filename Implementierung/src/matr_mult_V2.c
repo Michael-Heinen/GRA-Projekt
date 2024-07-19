eb0db9dd37fd87db413ab511a8ebdc77297e2457
Luis Waller <luis.waller@tum.de>
2024-07-19 13:15:37 +0200

    first simd implementation

--- Implementierung/src/matr_mult_V2.c
+++ Implementierung/src/matr_mult_V2.c
@@ -1,6 +1,7 @@
 #include "ellpack.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <immintrin.h>
 
 void matr_mult_ellpack_V2(const ELLPACKMatrix *a, const ELLPACKMatrix *b, ELLPACKMatrix *result)
 {
@@ -26,6 +27,7 @@ void matr_mult_ellpack_V2(const ELLPACKMatrix *a, const ELLPACKMatrix *b, ELLPAC
     // temporary result arrays for current row
     float *temp_values = (float *)calloc(result->noCols, sizeof(float));
     uint64_t *temp_indices = (uint64_t *)calloc(result->noCols, sizeof(uint64_t));
+    float *b_temp_values = (float *)calloc(b->noCols, sizeof(float));
     // uint64_t temp_nonZero = 0;
 
     for (uint64_t curr_a_row = 0; curr_a_row < a->noRows; ++curr_a_row)
@@ -34,49 +36,66 @@ void matr_mult_ellpack_V2(const ELLPACKMatrix *a, const ELLPACKMatrix *b, ELLPAC
         for (uint64_t curr_a_nonZero = 0; curr_a_nonZero < a->noNonZero; ++curr_a_nonZero)
         {
             uint64_t a_index = curr_a_row * a->noNonZero + curr_a_nonZero;
-            float a_value = a->values[a_index];
-            if (a_value == 0.0)
-            {
-                continue;
-            }
+            __m128 a_values = _mm_set1_ps(a->values[a_index]);
+            // if (_mm_testz_ps(a_values, a_values))
+            // {
+            //     continue; // Skip if all values in the SIMD register are zero
+            // }
             uint64_t a_col = a->indices[a_index];
 
-            for (uint64_t curr_b_nonZero = 0; curr_b_nonZero < b->noNonZero; ++curr_b_nonZero)
+            // load b row into temp b array
+            uint64_t b_row_base_index = a_col * b->noNonZero;
+
+            // loop over row of b corresponding to the a_col
+            for (uint64_t b_in = 0; b_in < b->noNonZero; b_in++)
             {
-                uint64_t b_index = a_col * b->noNonZero + curr_b_nonZero;
-                float b_value = b->values[b_index];
-                if (b_value == 0.0f)
+                uint64_t cur_b_index = b_row_base_index + b_in;
+                uint64_t b_col = b->indices[cur_b_index];
+                b_temp_values[b_col] = b->values[cur_b_index];
+                temp_indices[b_col] = b_col;
+            }
+
+            // loop over b_temp_array and calculate into temp_array
+            for (uint64_t curr_b_nonZero = 0; curr_b_nonZero < b->noNonZero; curr_b_nonZero += 4)
+            {
+                uint64_t b_index = b_row_base_index + curr_b_nonZero;
+                __m128 b_values = _mm_loadu_ps(&b_temp_values[curr_b_nonZero]);
+                // if (_mm_testz_ps(b_values, b_values))
+                // {
+                //     continue; // Skip if all values in the SIMD register are zero
+                // }
+                __m128 result_simd = _mm_mul_ps(a_values, b_values);
+                __m128 temp_simd = _mm_load_ps(&temp_values[curr_b_nonZero]);
+                temp_simd = _mm_add_ps(result_simd, temp_simd);
+
+                // load back into temp array
+                _mm_store_ps(&b_temp_values[curr_b_nonZero], temp_simd);
+            }
+
+            // copy temp arrays into final array
+            uint64_t cnt_zero = 0;
+            for (uint64_t i = 0; i < result->noCols; ++i)
+            {
+                if (temp_values[i] != 0.0f)
                 {
+                    uint64_t res_index = curr_a_row * result->noNonZero + i - cnt_zero;
+                    result->values[res_index] = temp_values[i];
+                    result->indices[res_index] = temp_indices[i];
                     continue;
                 }
-                uint64_t b_col = b->indices[b_index];
-
-                temp_values[b_col] += a_value * b_value;
-                temp_indices[b_col] = b_col;
-                // temp_nonZero++;
+                cnt_zero++;
             }
-        }
 
-        // copy temp arrays into final array
-        for (uint64_t i = 0; i < result->noCols; ++i)
-        {
-            if (temp_values[i] != 0.0f)
+            // reset temp arrays
+            for (uint64_t i = 0; i < result->noCols; ++i)
             {
-                uint64_t res_index = curr_a_row * result->noNonZero + i;
-                result->values[res_index] = temp_values[i];
-                result->indices[res_index] = temp_indices[i];
+                temp_values[i] = 0.0f;
+                temp_indices[i] = 0;
             }
+            // temp_nonZero = 0;
         }
 
-        // reset temp arrays
-        for (uint64_t i = 0; i < result->noCols; ++i)
-        {
-            temp_values[i] = 0.0f;
-            temp_indices[i] = 0;
-        }
-        // temp_nonZero = 0;
+        free(temp_values);
+        free(temp_indices);
     }
-
-    free(temp_values);
-    free(temp_indices);
 }
